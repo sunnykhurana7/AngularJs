a7d3a9c6c5f4ac37da36500b993177a23ac7a129
Sunny Khurana <sunny.khurana@tothenew.com>
2017-03-12 13:24:46 +0530

    addanothercontroller

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ var myApp = angular.module("myModule",[]);
 myApp.controller("myController",function($scope){
   $scope.message = "AngularJS Tutorials ";
 });
+
+
+// made another controller and binding with the module
+myApp.controller("mySecondController",function($scope){
+
+    // create a object
+    var employee = {
+      firstname  : "Sunny",
+      lastname : "khurana",
+      gender : "male"
+    };
+
+    $scope.employee = employee;
+})
