30d669414ac94596af8096ce13f0293c80d37067
Sunny Khurana <sunny.khurana@tothenew.com>
2017-03-12 15:17:15 +0530

    AddControllerinOneFunctionality

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ myApp.controller("mySecondController",function($scope){
 
     $scope.employee = employee;
 })
+
+// 
+// // use the method chaining to create the module and create the controller n bind the controller with module in one line
+// var myApp = angular
+//             .module("myModule",[])
+//             .controller("myController", function($scope){
+//
+//                 // create a object
+//                 var employee = {
+//                   firstname  : "Sunny",
+//                   lastname : "khurana",
+//                   gender : "male"
+//                 };
+//
+//                 $scope.employee = employee;
+//             });
