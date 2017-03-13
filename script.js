5ff3b0afd2a7973e3c3982315c83cbe4fe48b2da
Sunny Khurana <sunny.khurana@tothenew.com>
2017-03-13 23:39:21 +0530

    search filter use

--- script.js
+++ script.js
@@ -187,3 +187,23 @@ myApp.controller("Sorting",function($scope){
   $scope.workers = workers;
   $scope.sortcolumn = "firstname";
 });
+
+
+
+// controller for search filter in AngularJS
+myApp.controller("SearchController",function($scope){
+
+
+    var workers = [
+
+      { firstname:"Ben",dateOfBirth:new Date("November 23, 1980"),lastname:"hastings",gender:"male",salary:55000 },
+      { firstname:"Sara",dateOfBirth:new Date("November 23, 1980"),lastname:"paul",gender:"female",salary:60000 },
+      { firstname:"Mark",dateOfBirth:new Date("November 23, 1980"),lastname:"holland",gender:"male",salary:45000 },
+      { firstname:"pam",dateOfBirth:new Date("November 23, 1980"),lastname:"macintosh",gender:"female",salary:75000 },
+      { firstname:"Todd",dateOfBirth:new Date("November 23, 1980"),lastname:"barber",gender:"male",salary:89000 }
+
+    ];
+
+    $scope.workers = workers;
+
+});
