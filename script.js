99ca230f38340ed1ab22542e0c16fd6b3bbe818c
Sunny Khurana <sunny.khurana@tothenew.com>
2017-03-13 07:56:42 +0530

    ng-repeat

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 // create a module
-
-var myApp = angular.module("myModule",[]);
+var myApp = angular.module("myModule", []);
 
 // create a controller and register with the module
-myApp.controller("myController",function($scope){
-  $scope.message = "AngularJS Tutorials ";
+myApp.controller("myController", function($scope) {
+    $scope.message = "AngularJS Tutorials ";
 });
 
 
 // made another controller and binding with the module
-myApp.controller("mySecondController",function($scope){
+myApp.controller("mySecondController", function($scope) {
 
     // create a object
     var employee = {
-      firstname  : "Sunny",
-      lastname : "khurana",
-      gender : "male"
+        firstname: "Sunny",
+        lastname: "khurana",
+        gender: "male"
     };
 
     $scope.employee = employee;
@@ -40,22 +39,53 @@ myApp.controller("mySecondController",function($scope){
 
 // made another controller with images
 
-myApp.controller("imagecontroller",function($scope){
+myApp.controller("imagecontroller", function($scope) {
 
-  var country = {
-    name : "USA",
-    capital: "Washington",
-    flag: "Flag_of_the_United_States.svg.png"
-  };
+    var country = {
+        name: "USA",
+        capital: "Washington",
+        flag: "Flag_of_the_United_States.svg.png"
+    };
 
-  $scope.country = country;
+    $scope.country = country;
 
 });
 
 
 
 //controller for two-way data binding
-myApp.controller("two-wayBinding",function($scope){
+myApp.controller("two-wayBinding", function($scope) {
 
-  $scope.message = "Hello AngularJS!"
+    $scope.message = "Hello AngularJS!"
 })
+
+
+
+// controller for two way data binding use in a table
+myApp.controller("TableController", function($scope) {
+
+    var Collegename = {
+        name: "jims",
+        place: "rohini",
+        course: "mca"
+    };
+
+    $scope.Collegename = Collegename;
+
+})
+
+
+// controller for learn the concept of ng-repeat directive
+myApp.controller("NGRepeat",function($scope){
+
+  var employees = [
+    { firstname:"Ben",lastname:"hastings",gender:"male",salary:55000 },
+    { firstname:"Sara",lastname:"paul",gender:"female",salary:60000 },
+    { firstname:"Mark",lastname:"holland",gender:"male",salary:45000 },
+    { firstname:"pam",lastname:"macintosh",gender:"female",salary:75000 },
+    { firstname:"Todd",lastname:"barber",gender:"male",salary:89000 }
+   ]
+
+   $scope.employees = employees;
+
+});
